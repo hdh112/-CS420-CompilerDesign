9b146617b18b153f9c14c307ac564fe248bab428
Do Heon Hwang <avohdh112@gmail.com>
2018-09-24 18:09:56 +0900

    pre_order not working, list index out of range

--- Homework1/PredictiveParser.py
+++ Homework1/PredictiveParser.py
@@ -2,65 +2,54 @@
 class Node(object):
     def __init__(self, type, val=None, child1=None, child2=None):
         self.type=type
-        self.val=val
-        self.ch1=child1
-        self.ch2=child2
-
-    def type(self):
-        return self.type
-
-    def value(self):
-        return self.val
-
-    def child1(self):
-        return self.ch1
-
-    def child2(self):
-        return self.ch2
+        self.value=val
+        self.child1=child1
+        self.child2=child2
 
 ###Functions to construct non-terminals###
-def parse_factor(str):
-    tok = str.pop(0)
+def parse_factor(tokens):
+    tok = tokens.pop(0)
     if tok.isalpha():   # identifier
         return Node(type="id", val=tok)
     else:               # number
-        while str[0].isdigit():
-            tok.append(str.pop(0))  # make token multiple-digit number
-        return Node(type="num", val=tok)
+        while tokens[0].isdigit():
+            tok = list(tok)             # change token into mutable type
+            tok.append(tokens.pop(0))   # make token multiple-digit number
+        return Node(type="num", val=''.join(tok))
 
-def parse_term_pr(str):
-    if len(str)>0:
-        tok = str[0]    # one lookahead
+def parse_term_pr(tokens):
+    if len(tokens)>0:
+        tok = tokens[0]    # one lookahead
         if tok == '*' or tok == '/':
-            tok = str.pop(0)
-            return Node(type='op', val=tok, child1=parse_term(str))
+            tok = tokens.pop(0)
+            return Node(type='op', val=tok, child1=parse_term(tokens))
     else:
         return Node(type='empty')
 
-def parse_term(str):
-    return Node(type="term", child1=parse_factor(str), child2=parse_term_pr(str))
+def parse_term(tokens):
+    return Node(type="term", child1=parse_factor(tokens), child2=parse_term_pr(tokens))
 
-def parse_expr_pr(str):
-    if len(str)>0:
-        tok = str[0]    # one lookahead
+def parse_expr_pr(tokens):
+    if len(tokens)>0:
+        tok = tokens[0]    # one lookahead
         if tok == '+' or tok == '-':
-            tok = str.pop(0)
-            return Node(type='op', val=tok, child1=parse_term(str), child2=parse_expr_pr)
+            tok = tokens.pop(0)
+            return Node(type='op', val=tok, child1=parse_term(tokens), child2=parse_expr_pr(tokens))
     else:
         return Node(type='empty')
 
-def parse_expr(str):
-    return Node(type="expr", child1=parse_term(str), child2=parse_expr_pr(str))
+def parse_expr(tokens):
+    return Node(type="expr", child1=parse_term(tokens), child2=parse_expr_pr(tokens))
 ##########################################
 
 '''Helper function to eliminate white space in input string line
                     & change into mutable data type(list)'''
-def tokenize(str):
-    return list(str.rstrip().replace(" ",""))
+def tokenize(str_line):
+    return list(str_line.rstrip().replace(" ",""))
 
 '''Parse the given input string line'''
-def parser(str):
-    tokens = tokenize(str)
+def parser(str_line):
+    tokens = tokenize(str_line)
     if len(tokens)==0:
         return "incorrect syntax"
     return parse_expr(tokens)
@@ -72,11 +61,14 @@ def merge(str1, str2, str3):
 
 '''Express the parsed tree in pre_order syntax string'''
 def pre_order(root):
-    if root.type() in ["id", "num", "op"]:
-        val = root.value()
-    else:
-        val = ''
-    return merge(val, pre_order(root.child1()), pre_order(root.child2()))
+    try:
+        if root.type in ["id", "num", "op"]:
+            val = root.value
+        else:
+            val = ''
+        return merge(val, pre_order(root.child1), pre_order(root.child2))
+    except AttributeError:  # root is NoneType
+        return ''
 
 
 def main():
@@ -88,7 +80,7 @@ def main():
     for line in file_in:
         expr_result = pre_order(parser(line))
         print(expr_result)
-        file_out.write(expr_result)
+        file_out.write(expr_result+'\n')
 
     file_in.close()
     file_out.close()
